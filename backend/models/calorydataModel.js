bfe39c51586f8cb871eae75dbd664084c3d66964
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2022-08-05 16:25:24 +0100

    :chart_with_upwards_trend: model for the database structure

---
+++ backend/models/calorydataModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const caloryDatasSchema = new Schema(
+  {
+    food: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    meal: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    calories: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) throw new Error("Negative calories aren't real.");
+      },
+    },
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model("CaloryData", caloryDatasSchema);
+
+// user_id: {
+//   type: String,
+//   required: true,
+// },
