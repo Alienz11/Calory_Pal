d8dc9836747a6d7caf214161cd9a7c2e252b019d
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2022-08-05 12:20:54 +0100

    Login and Sign up page with animated display

---
+++ frontend/src/components/animationBox/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import {
+  Container,
+  BoldLink,
+  FormContainer,
+  Input,
+  MutedLink,
+  SubmitButton,
+} from "./Collective";
+import { Marginer } from "../marginer/marginMaker";
+
+import { AnimationContext } from "./animationContext";
+
+export function LoginForm(props) {
+  const { switchToSignup } = useContext(AnimationContext);
+
+  return (
+    <Container>
+      <FormContainer>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+      </FormContainer>
+      <Marginer direction="vertical" margin={10} />
+      <MutedLink href="#">Forget your password?</MutedLink>
+      <Marginer direction="vertical" margin="1.6em" />
+      <SubmitButton type="submit">Login</SubmitButton>
+      <Marginer direction="vertical" margin="1em" />
+      <MutedLink href="#">
+        Don't have an account?{" "}
+        <BoldLink href="#" onClick={switchToSignup}>
+          Signup
+        </BoldLink>
+      </MutedLink>
+    </Container>
+  );
+}
