6920b4a7f2eafc95ee613465056b61a33e631130
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2022-08-06 16:57:32 +0100

    :arrows_counterclockwise: Updated app file by adding routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import styled from "styled-components";
 import { AnimationBox } from "./components/animationBox/Animation";
+import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
+import HomePage from "./pages/HomePage";
 
 const AppContainer = styled.div`
   height: 50em;
@@ -10,11 +14,50 @@ const AppContainer = styled.div`
 `;
 
 function App() {
+  const { user } = useAuthContext();
+  console.log(user);
   return (
-    <AppContainer>
-      <AnimationBox />
-    </AppContainer>
+    <BrowserRouter>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={
+              !user ? (
+                <AppContainer>
+                  <AnimationBox />
+                </AppContainer>
+              ) : (
+                <Navigate to="/" />
+              )
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// function App() {
+//   const { user } = useAuthContext();
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <div>
+//         <Routes>
+//           <Route path="/" element={user ? <HomePage /> : <Navigate to="/" />} />
+//         </Routes>
+//       </div>
+//       <AppContainer>
+//         <AnimationBox />
+//       </AppContainer>
+//     </BrowserRouter>
+//   );
+// }
