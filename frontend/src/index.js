7127f840fb0fd396995696f02039b5c9ffa920ab
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2022-08-07 08:05:12 +0100

    :broom: clean-up for index file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { CaloryContextProvider } from "./context/CaloryContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <CaloryContextProvider>
+        <App />
+      </CaloryContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
-
